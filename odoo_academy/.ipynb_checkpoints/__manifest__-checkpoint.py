6f67ca5a7496ad8aae2cfde6d1c62ecdf4062fb0
akkd-odoo <akkd@odoo.com>
2021-08-02 23:51:23 +0000

    [ADD] Basic Module

---
+++ odoo_academy/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+{
+    
+    "name": "akkd ACADEMY",
+    
+    'Summary': """This is an academy app to manage training""",
+    
+    'description': """
+    
+        Academy Module to manage Trainings:
+        - Courses
+        - Sessions
+        - Attendees
+    """,
+    
+    'author': "Odoo",
+    
+    'website': "https://odoo.com",
+    
+    'category': 'training',
+    
+    'version': '0.1',
+    
+    'depends': ['base'],
+    
+    'data': [
+        
+    ],
+    
+    'demo':[
+        
+        
+    ]
+    
+    
+}
