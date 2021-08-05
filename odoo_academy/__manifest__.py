48f2f8db57430be425e7b166214863566ea1a3f2
akkd-odoo <akkd@odoo.com>
2021-08-05 00:06:23 +0000

    [ADD] Advanced Fields

--- odoo_academy/__manifest__.py
+++ odoo_academy/__manifest__.py
@@ -25,13 +25,19 @@
     'depends': ['base'],
     
     'data': [
-        
+        'security/academy_security.xml',
+        'security/ir.model.access.csv',
+        'views/academy_menuitems.xml',
+        'views/course_views.xml',
+        'views/session_views.xml',
     ],
+    
     
     'demo':[
         
+        "demo/academy_demo.xml",
         
-    ]
+    ],
     
     
 }
