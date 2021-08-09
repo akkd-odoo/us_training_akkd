8e6abf78c629976e38ea76cddda33533cf39220d
akkd-odoo <akkd@odoo.com>
2021-08-09 02:19:49 +0000

    [ADD] All

---
+++ odoo-academy/__manifest__.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Library Manager',
+    'summary': """Library app to manage all library needs""",
+    'description': """
+        Manage all aspects of your library:
+        - Books
+        - Memberships
+        - Rentals
+    """,
+    
+    'author': 'PCA',
+    'website': 'https://www.odoo.com',
+    'category': 'training',
+    'version': '0.1',
+    
+    'depends': ['base','web_map'],
+    'data': [
+        'security/library_groups.xml',
+        'security/library_security.xml',
+        'security/ir.model.access.csv',
+        'views/book_views.xml',
+        'views/rental_views.xml',
+        'views/library_menuitems.xml',
+        'views/book_copy_views.xml',
+        
+    ],
+    'demo': [
+        'demo/book_demo.xml',
+        
+    ],
+}
